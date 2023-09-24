42edb9098bd3c69dd8b5902a57ff3c7ad4b00fd4
Mikhail Korsun <mr.reduction@gmail.com>
2023-09-24 15:53:00 +0300

    Complete Task_003

---
+++ Seminars/Sem2/Task_003/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+void CheckMultiplicityto7and23(int number)
+{
+    if (number % 7 == 0 && number % 23 == 0)
+        Console.WriteLine("Число {0} одновременно кратно и 7, и 23)", number);
+    else
+        Console.WriteLine("Число {0} некратно одновременно 7 и 23)", number);
+}
+
+Console.Write("Введите целое число: ");
+int numberBeingChecked = Convert.ToInt32(Console.ReadLine());
+CheckMultiplicityto7and23(numberBeingChecked);
