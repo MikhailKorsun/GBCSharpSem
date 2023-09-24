41f7b6ff469c6f4f12e72e7f5d55c08da8b2f2f5
Mikhail Korsun <mr.reduction@gmail.com>
2023-09-24 15:38:03 +0300

    Complete Task_002

---
+++ Seminars/Sem2/Task_002/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая выводит 
+// случайное число из отрезка [10, 99] и показывает 
+// наибольшую цифру числа
+
+int FindBiggestDigit (int num)
+{
+    int ed = num % 10;
+    int dec = num / 10;
+    if (ed > dec)
+    {
+        return ed;
+    }
+    else
+    {
+        return dec;
+    }
+}
+
+
+int num = new Random().Next(10,100);
+Console.Write($"Сгенерированное число: {num}, а его наибольшая цифра ");
+Console.WriteLine(FindBiggestDigit(num));
