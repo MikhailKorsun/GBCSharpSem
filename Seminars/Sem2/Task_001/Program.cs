a1c4a0085b63b5777ab5d86d0db5bb545d3235d1
Mikhail Korsun <mr.reduction@gmail.com>
2023-09-24 15:05:34 +0300

    Complete Task_001

--- Seminars/Sem2/Task_001/Program.cs
+++ Seminars/Sem2/Task_001/Program.cs
@@ -1,4 +1,26 @@
-﻿// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если второе число не кратно числу первому, то программа выводит остаток от деления.
+﻿// Напишите программу, которая будет принимать на вход два числа
+// и выводить, является ли первое число кратным второму.
+// Если первое число не кратно второму числу, то программа выводит остаток от деления.
 
 // 34, 5 -> не кратно, остаток 4
 // 16, 4 -> кратно
+
+void CheckMultiplicity (int num1, int num2)
+{
+    if (num1 % num2 == 0)
+    {
+        Console.WriteLine($"{num1} кратно {num2}");
+    }
+    else
+    {
+        Console.WriteLine($"{num1} не кратно {num2}, остаток от деления: {num1 % num2}");
+    }
+}
+
+Console.WriteLine("Введите два целых числа для проверки: ");
+Console.Write("Первое: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Второе: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+CheckMultiplicity(firstNumber,secondNumber);
