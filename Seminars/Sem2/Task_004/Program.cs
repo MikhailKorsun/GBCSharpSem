1332527933adcb44c0fdfb0b65e68e0f2dbca029
Mikhail Korsun <mr.reduction@gmail.com>
2023-09-24 16:11:01 +0300

    Complete Task_004

---
+++ Seminars/Sem2/Task_004/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+int DeleteSecondDigit(int number)
+{
+    int ed = number % 10;
+    int hundred = number / 100;
+    int result = hundred * 10 + ed;
+    return result;
+}
+
+int num = new Random().Next(100,1000);
+Console.Write($"Сгенерированное число: {num}, новое число: ");
+Console.WriteLine(DeleteSecondDigit(num));
